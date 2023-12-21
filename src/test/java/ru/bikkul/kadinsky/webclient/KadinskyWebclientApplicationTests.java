e6fca5233514c10c80f87990dff1f03e7fb99818
quadalus <quadalus@yandex.ru>
2023-12-22 00:39:12 +0300

    feat: init project

---
+++ src/test/java/ru/bikkul/kadinsky/webclient/KadinskyWebclientApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bikkul.kadinsky.webclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KadinskyWebclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
